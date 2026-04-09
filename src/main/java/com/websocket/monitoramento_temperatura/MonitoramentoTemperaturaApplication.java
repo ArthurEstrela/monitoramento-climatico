8b969e9aa9f72a0830b440a81d3e8b8b4e88ba5c
Arthur Faria Estrela <arthurestrela369@gmail.com>
2026-04-09 07:43:58 -0300

    first commit

---
+++ src/main/java/com/websocket/monitoramento_temperatura/MonitoramentoTemperaturaApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.monitoramento_temperatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoTemperaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoTemperaturaApplication.class, args);
+	}
+
+}
